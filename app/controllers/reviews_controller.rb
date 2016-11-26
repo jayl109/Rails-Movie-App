31af3cb879cfaae6ff7ab0bbff539d7d7e9bb60b
Nathan Khuu <nkhuu@berkeley.edu>
2016-11-26 14:10:57 -0800

    Review for specific movie, display reviews on movie pages

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,15 +11,16 @@ class ReviewsController < ApplicationController
 
 	def new
 		@review = Review.new
+		@movie_id = params[:id]
 	end
 
 	def create
 		@review = Review.new
 		@review.rating = params[:review][:rating]
 		@review.text = params[:review][:text]
-		@review.critic_id = current_viewer.id
-		@review.movie_id = :id
+		@review.critic_id = Critic.first.id #current_viewer.id
+		@review.movie_id = params[:movie_id]
 		@review.save
-		redirect_to "/reviews/index"
+		redirect_to "/movies"
 	end
 end
