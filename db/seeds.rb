27997c2d0d3b424d78a5360c1ff4efff3cf4b57a
Matthew Li <matthew.li@berkeley.edu>
2016-11-27 18:30:47 -0800

    Additional styling, organizational changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,16 @@
 #
 # Examples:
 #
-Movie.create(name: 'Star Wars', avg_rating: 100)
+Movie.create(
+	name: 'Star Wars: Episode IV - A New Hope',
+	avg_rating: 100,
+	description: "The Imperial Forces -- under orders from cruel Darth Vader (David Prowse) -- hold Princess "\
+				"Leia (Carrie Fisher) hostage, in their efforts to quell the rebellion against the Galactic "\
+				"Empire. Luke Skywalker (Mark Hamill) and Han Solo (Harrison Ford), captain of the Millennium "\
+				"Falcon, work together with the companionable droid duo R2-D2 (Kenny Baker) and C-3PO (Anthony "\
+				"Daniels) to rescue the beautiful princess, help the Rebel Alliance, and restore freedom and "\
+				"justice to the Galaxy."
+	)
 Movie.create(name: 'Transformers', avg_rating: 0)
 Critic.create(name: 'Joe', bio: "I'm Joe. I like movies.")
 Critic.create(name: 'Rob', bio: "I'm Rob. I like movies.")
